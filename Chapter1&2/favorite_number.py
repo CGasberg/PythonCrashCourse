fbab4d5985c919bcedd7158814b19690beef560f
Gasberg <christian.gasberg@accenture.com>
2017-02-20 21:58:03 +0100

    More structure

---
+++ Chapter1&2/favorite_number.py
@@ -0,0 +1,4 @@
+#Christian G 20-02-2017
+#This program prints my favorite number
+fav_number = 15
+print("My Favorit number is " + str(fav_number) +"!")
---
+++ Chapter1&2/simple_messages.py
@@ -0,0 +1,11 @@
+message = "Hello to you my dearest friend!"
+print(message)
+
+message = "You are not my fried!"
+print(message)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print ("Hello, " + full_name.title() + "!")
