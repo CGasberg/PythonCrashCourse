24314fa25f3866042637f065e996ad111873c8af
Gasberg <christian.gasberg@accenture.com>
2017-02-20 21:55:41 +0100

    Created some structure and started working on chapter 3

---
+++ Chapter3/greetings.py
@@ -0,0 +1,3 @@
+names = ["Christian","Lasse","Marcus","Brian"]
+for name in names:
+    print ("Hello my dearest friend %s" % name)
---
+++ Chapter3/names.py
@@ -0,0 +1,3 @@
+names = ["Christian","Lasse","Marcus","Brian"]
+for name in names:
+    print (name)
