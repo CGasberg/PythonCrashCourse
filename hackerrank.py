abb8c03183ae7097aac41d5fabea416fe0e54af4
Christian Gasberg <cgasberg@gmail.com>
2017-02-26 21:22:20 +0100

    Added 5 new excersise fiels and 2 fool around files

---
+++ hackerrank.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+    N = int(input())
+    list = []
+
+
+def insert(x,y):
+    list.insert(x,y)
+
+
+print list
+
+def remove(x):
+    list.remove(x)
+
+
+print list
+
+def append(x):
+    list.append(x)
+
+def sort():
+    list.sort()
+
+def pop():
+    list.pop()
+
+def reverse():
+    list.reverse()
