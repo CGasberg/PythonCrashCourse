3d4b838cf1f564c80b29d0df2095ca634221c231
Gasberg <christian.gasberg@accenture.com>
2017-03-12 14:14:19 +0100

    More insults

--- Insults/shakespeare.py
+++ Insults/shakespeare.py
@@ -2,6 +2,7 @@
 #    contents = f.read()
 #    print(contents)
 import random
+import csv
 
 list_a = []
 list_b = []
@@ -22,4 +23,30 @@ def insult_me():
     insult = "Thou " + word_a + " "+ word_b + " " + word_c + "!"
     print(insult)
 
+    rating = input("Please rate your insult from 1 - 5: ")
+    print("You've given the insult: " + insult + " the rating " + rating + "!")
+
+    writer = csv.writer(open("ratings.csv", "a", newline=""))
+    writer.writerow([insult.strip() + ";" + rating.strip()])
+
+
 insult_me()
+
+def display_rating():
+    display = input("Would you like to display the most rated insult? Y/N")
+    if display == "Y":
+        maxrate = 0
+        with open("ratings.csv","r") as f:
+            next(f)
+            try:
+                m = int(column[1])
+                maxrate = max(maxrate,m)
+            except:
+                pass
+
+        print (maxrate)
+
+    else:
+        print("Have a nice day")
+
+display_rating()
